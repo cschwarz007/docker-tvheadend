e836826a0b77210f6aa296fe05df30e36e72672d
Hetsh <git@hetsh.de>
2020-02-19 17:17:55 +0100

    Use simple update functions

--- update.sh
+++ update.sh
@@ -17,37 +17,13 @@ assert_dependency "jq"
 assert_dependency "curl"
 
 # Current version of docker image
-CURRENT_VERSION=$(git describe --tags --abbrev=0)
-register_current_version "$CURRENT_VERSION"
+register_current_version
 
 # Alpine Linux
-IMAGE_PKG="alpine"
-IMAGE_NAME="Alpine"
-IMAGE_REGEX="(\d+\.)+\d+"
-IMAGE_TAGS=$(curl -L -s "https://registry.hub.docker.com/v2/repositories/library/$IMAGE_PKG/tags" | jq '."results"[]["name"]' | grep -P -o "$IMAGE_REGEX")
-IMAGE_VERSION=$(echo "$IMAGE_TAGS" | sort --version-sort | tail -n 1)
-CURRENT_IMAGE_VERSION=$(cat "Dockerfile" | grep -P -o "FROM $IMAGE_PKG:\K$IMAGE_REGEX")
-if [ "$CURRENT_IMAGE_VERSION" != "$IMAGE_VERSION" ]; then
-	echo "$IMAGE_NAME $IMAGE_VERSION available!"
-	update_release
-fi
+update_image "alpine" "Alpine" "x86_64" "(\d+\.)+\d+"
 
 # TV-Headend
-TVH_PKG="tvheadend"
-TVH_NAME="TV-Headend"
-TVH_REGEX="(\d+\.)+\d+-r\d+"
-TVH_VERSION=$(curl -L -s "https://pkgs.alpinelinux.org/package/v${IMAGE_VERSION%.*}/community/x86_64/$TVH_PKG" | grep -m 1 -P -o "$TVH_REGEX")
-CURRENT_TVH_VERSION=$(cat "Dockerfile" | grep -P -o "$TVH_PKG=\K$TVH_REGEX")
-if [ "$CURRENT_TVH_VERSION" != "$TVH_VERSION" ]; then
-	echo "$TVH_NAME $TVH_VERSION available!"
-
-	# Dont update version if only release counter changed
-	if [ "${CURRENT_TVH_VERSION%-*}" != "${TVH_VERSION%-*}" ]; then
-		update_version "${TVH_VERSION%-*}"
-	else
-		update_release
-	fi
-fi
+update_alpine_pkg "tvheadend" "TV-Headend" "true" "community" "(\d+\.)+\d+-r\d+"
 
 if ! updates_available; then
 	echo "No updates available."
@@ -56,17 +32,9 @@ fi
 
 # Perform modifications
 if [ "${1+}" = "--noconfirm" ] || confirm_action "Save changes?"; then
-	if [ "$CURRENT_IMAGE_VERSION" != "$IMAGE_VERSION" ]; then
-		sed -i "s|FROM $IMAGE_PKG:$CURRENT_IMAGE_VERSION|FROM $IMAGE_PKG:$IMAGE_VERSION|" Dockerfile
-		CHANGELOG+="$IMAGE_NAME $CURRENT_IMAGE_VERSION -> $IMAGE_VERSION, "
-	fi
-	if [ "$CURRENT_TVH_VERSION" != "$TVH_VERSION" ]; then
-		sed -i "s|$TVH_PKG=$CURRENT_TVH_VERSION|$TVH_PKG=$TVH_VERSION|" Dockerfile
-		CHANGELOG+="$TVH_NAME $CURRENT_TVH_VERSION -> $TVH_VERSION, "
-	fi
-	CHANGELOG="${CHANGELOG%,*}"
+	save_changes
 
 	if [ "${1+}" = "--noconfirm" ] || confirm_action "Commit changes?"; then
-		commit_changes "$CHANGELOG"
+		commit_changes
 	fi
 fi
