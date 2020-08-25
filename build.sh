b176aad6c1804a2297c8f554e19a3524dd12d3ce
Hetsh <git@hetsh.de>
2020-08-25 13:08:34 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,32 +21,26 @@ fi
 
 # Build the image
 APP_NAME="tvheadend"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+VERSION="$(git describe --tags --abbrev=0)"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$VERSION" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directories
-	TMP_DATA_DIR=$(mktemp -d "/tmp/$APP_NAME-DATA-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DATA_DIR"
-	TMP_REC_DIR=$(mktemp -d "/tmp/$APP_NAME-REC-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_REC_DIR"
-
-	# Apply permissions, UID matches process user
-	extract_var APP_UID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_UID" "$TMP_DATA_DIR" "$TMP_REC_DIR"
-
-	# Start the test
-	extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 9981:9981/tcp \
-	--publish 9982:9982/tcp \
-	--mount type=bind,source="$TMP_DATA_DIR",target="$DATA_DIR" \
-	--mount type=bind,source="$TMP_REC_DIR",target="/mnt/rec" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+case "${1-}" in
+	"--test")
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 9981:9981/tcp \
+		--publish 9982:9982/tcp \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
